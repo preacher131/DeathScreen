7213fdf860d8265de81c1d611c160d888c822123
Flatracer <flatracer@ymail.com>
2017-11-11 19:44:27 +0100

    Improvements

--- __resource.lua
+++ __resource.lua
@@ -3,5 +3,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Death Screen' -- Resource Descrption
 
 client_script {
-	'client/DeathScreen.lua',
+	'client/client.lua',
+}
+
+server_script {
+	'server/server.lua',
 }
