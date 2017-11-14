b810456fa42bf63f04f3e04149d120dd461ab1c9
Flatracer <flatracer@ymail.com>
2017-11-14 21:56:02 +0100

    Fix
    Fixed the problem, that it prints 'NULL'

--- client/client.lua
+++ client/client.lua
@@ -6,25 +6,25 @@ Citizen.CreateThread(function()
     while true do
        Citizen.Wait(0)  
 	   
-		if not HasThisAdditionalTextLoaded("global", 100) then
+		if not HasThisAdditionalTextLoaded('global', 100) then
 			ClearAdditionalText(100, true)
-			RequestAdditionalText("global", 100)
-			while not HasThisAdditionalTextLoaded("global", 100) do
+			RequestAdditionalText('global', 100)
+			while not HasThisAdditionalTextLoaded('global', 100) do
 				Citizen.Wait(0)
 			end
 		end
 		
-		if not HasNamedScaleformMovieLoaded("MP_BIG_MESSAGE_FREEMODE") then
-			scaleform = RequestScaleformMovie("MP_BIG_MESSAGE_FREEMODE")
-			while not HasNamedScaleformMovieLoaded("MP_BIG_MESSAGE_FREEMODE") do
+		if not HasNamedScaleformMovieLoaded('MP_BIG_MESSAGE_FREEMODE') then
+			scaleform = RequestScaleformMovie('MP_BIG_MESSAGE_FREEMODE')
+			while not HasNamedScaleformMovieLoaded('MP_BIG_MESSAGE_FREEMODE') do
 				Citizen.Wait(0)
 			end
 		end
-
+		
 		if IsPlayerDead(PlayerId()) then
 			local PedKiller = GetPedKiller(PlayerPedId())
 			local DeathCauseHash = GetPedCauseOfDeath(PlayerPedId())
-			local Killer, DeathReason
+			local Killer, DeathReason, INT
 			local KilledPlayer = PlayerId()
 			
 			if IsPedAPlayer(PedKiller) then
@@ -33,73 +33,83 @@ Citizen.CreateThread(function()
 				Killer = nil
 			end
 			
-
 			if (Killer == PlayerId()) then
-				DeathReason = GetLabelText("DM_U_SUIC")
+				DeathReason = GetLabelText('DM_U_SUIC')
 			elseif (Killer == nil) then
-				DeathReason = GetLabelText("DM_TK_YD1")
+				DeathReason = GetLabelText('DM_TK_YD1')
 			else
-				if DeathCauseHash == GetHashKey("WEAPON_CROWBAR") or DeathCauseHash == GetHashKey("WEAPON_BAT") or 
-				   DeathCauseHash == GetHashKey("WEAPON_UNARMED") or  DeathCauseHash == GetHashKey("WEAPON_GOLFCLUB") or
-				   DeathCauseHash == GetHashKey("WEAPON_HAMMER") or  DeathCauseHash == GetHashKey("WEAPON_NIGHTSTICK") then
-					DeathReason = "DM_TK_MELEE1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_MOLOTOV") then
-					DeathReason = "DM_TK_TORCH1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_DAGGER") or DeathCauseHash == GetHashKey("WEAPON_KNIFE") or
-					   DeathCauseHash == GetHashKey("WEAPON_SWITCHBLADE") or DeathCauseHash == GetHashKey("WEAPON_HATCHET") or
-					   DeathCauseHash == GetHashKey("WEAPON_BOTTLE") then
-					DeathReason = "DM_TK_TORCH1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_SNSPISTOL") or DeathCauseHash == GetHashKey("WEAPON_HEAVYPISTOL") or
-					   DeathCauseHash == GetHashKey("WEAPON_VINTAGEPISTOL") or DeathCauseHash == GetHashKey("WEAPON_PISTOL") or
-					   DeathCauseHash == GetHashKey("WEAPON_APPISTOL") or DeathCauseHash == GetHashKey("WEAPON_COMBATPISTOL") then
-					DeathReason = "DM_TK_PISTOL1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_MICROSMG") or DeathCauseHash == GetHashKey("WEAPON_SMG") then
-					DeathReason = "DM_TK_SUB1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_CARBINERIFLE") or DeathCauseHash == GetHashKey("WEAPON_MUSKET") or
-					   DeathCauseHash == GetHashKey("WEAPON_ADVANCEDRIFLE") or DeathCauseHash == GetHashKey("WEAPON_ASSAULTRIFLE") or
-					   DeathCauseHash == GetHashKey("WEAPON_SPECIALCARBINE") or DeathCauseHash == GetHashKey("WEAPON_COMPACTRIFLE") or
-					   DeathCauseHash == GetHashKey("WEAPON_BULLPUPRIFLE") then
-					DeathReason = "DM_TK_ARIFLE1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_MG") or DeathCauseHash == GetHashKey("WEAPON_COMBATMG") then
-					DeathReason = "DM_TK_LIGHT1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_BULLPUPSHOTGUN") or DeathCauseHash == GetHashKey("WEAPON_ASSAULTSHOTGUN") or
-					   DeathCauseHash == GetHashKey("WEAPON_DBSHOTGUN") or DeathCauseHash == GetHashKey("WEAPON_PUMPSHOTGUN") or
-					   DeathCauseHash == GetHashKey("WEAPON_HEAVYSHOTGUN") or DeathCauseHash == GetHashKey("WEAPON_SAWNOFFSHOTGUN") then
-					DeathReason = "DM_TK_SHOT1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_MARKSMANRIFLE") or DeathCauseHash == GetHashKey("WEAPON_SNIPERRIFLE") or
-					   DeathCauseHash == GetHashKey("WEAPON_HEAVYSNIPER") or DeathCauseHash == GetHashKey("WEAPON_ASSAULTSNIPER") or
-					   DeathCauseHash == GetHashKey("WEAPON_REMOTESNIPER") then
-					DeathReason = "DM_TK_SNIPE1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_GRENADELAUNCHER") or DeathCauseHash == GetHashKey("WEAPON_RPG") or
-					   DeathCauseHash == GetHashKey("WEAPON_FLAREGUN") or DeathCauseHash == GetHashKey("WEAPON_HOMINGLAUNCHER") or
-					   DeathCauseHash == GetHashKey("WEAPON_FIREWORK") then
-					DeathReason = "DM_TK_HEAVY1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_MINIGUN") then
-					DeathReason = "DM_TK_MINI1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_GRENADE") or DeathCauseHash == GetHashKey("WEAPON_PROXMINE") or
-					   DeathCauseHash == GetHashKey("WEAPON_EXPLOSION") or DeathCauseHash == GetHashKey("WEAPON_STICKYBOMB") then
-					DeathReason = "DM_TK_BOMB1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_BZGAS") then
-					DeathReason = "DM_TK_GAS1"
-				elseif DeathCauseHash == GetHashKey("VEHICLE_WEAPON_TANK") then
-					DeathReason = "DM_TK_HEAVY1"
-				elseif DeathCauseHash == GetHashKey("VEHICLE_WEAPON_ROTORS") then
-					DeathReason = "DM_TK_VEH1"
-				elseif DeathCauseHash == GetHashKey("WEAPON_RUN_OVER_BY_CAR") or DeathCauseHash == GetHashKey("WEAPON_RAMMED_BY_CAR") then
-					DeathReason = "DM_TK_VK1"
+				if DeathCauseHash == GetHashKey('WEAPON_CROWBAR') or DeathCauseHash == GetHashKey('WEAPON_BAT') or 
+				   DeathCauseHash == GetHashKey('WEAPON_UNARMED') or  DeathCauseHash == GetHashKey('WEAPON_GOLFCLUB') or
+				   DeathCauseHash == GetHashKey('WEAPON_HAMMER') or  DeathCauseHash == GetHashKey('WEAPON_NIGHTSTICK') then
+					INT = GetRandomIntInRange(0, 4)
+					DeathReason = 'DM_TK_MELEE1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_MOLOTOV') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_TORCH1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_DAGGER') or DeathCauseHash == GetHashKey('WEAPON_KNIFE') or
+					   DeathCauseHash == GetHashKey('WEAPON_SWITCHBLADE') or DeathCauseHash == GetHashKey('WEAPON_HATCHET') or
+					   DeathCauseHash == GetHashKey('WEAPON_BOTTLE') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_KNIFE1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_SNSPISTOL') or DeathCauseHash == GetHashKey('WEAPON_HEAVYPISTOL') or
+					   DeathCauseHash == GetHashKey('WEAPON_VINTAGEPISTOL') or DeathCauseHash == GetHashKey('WEAPON_PISTOL') or
+					   DeathCauseHash == GetHashKey('WEAPON_APPISTOL') or DeathCauseHash == GetHashKey('WEAPON_COMBATPISTOL') then
+					INT = GetRandomIntInRange(0, 1)
+					DeathReason = 'DM_TK_PISTOL1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_MICROSMG') or DeathCauseHash == GetHashKey('WEAPON_SMG') then
+					INT = GetRandomIntInRange(0, 3)
+					DeathReason = 'DM_TK_SUB1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_CARBINERIFLE') or DeathCauseHash == GetHashKey('WEAPON_MUSKET') or
+					   DeathCauseHash == GetHashKey('WEAPON_ADVANCEDRIFLE') or DeathCauseHash == GetHashKey('WEAPON_ASSAULTRIFLE') or
+					   DeathCauseHash == GetHashKey('WEAPON_SPECIALCARBINE') or DeathCauseHash == GetHashKey('WEAPON_COMPACTRIFLE') or
+					   DeathCauseHash == GetHashKey('WEAPON_BULLPUPRIFLE') then
+					INT = GetRandomIntInRange(0, 3)
+					DeathReason = 'DM_TK_ARIFLE1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_MG') or DeathCauseHash == GetHashKey('WEAPON_COMBATMG') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_LIGHT1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_BULLPUPSHOTGUN') or DeathCauseHash == GetHashKey('WEAPON_ASSAULTSHOTGUN') or
+					   DeathCauseHash == GetHashKey('WEAPON_DBSHOTGUN') or DeathCauseHash == GetHashKey('WEAPON_PUMPSHOTGUN') or
+					   DeathCauseHash == GetHashKey('WEAPON_HEAVYSHOTGUN') or DeathCauseHash == GetHashKey('WEAPON_SAWNOFFSHOTGUN') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_SHOT1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_MARKSMANRIFLE') or DeathCauseHash == GetHashKey('WEAPON_SNIPERRIFLE') or
+					   DeathCauseHash == GetHashKey('WEAPON_HEAVYSNIPER') or DeathCauseHash == GetHashKey('WEAPON_ASSAULTSNIPER') or
+					   DeathCauseHash == GetHashKey('WEAPON_REMOTESNIPER') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_SNIPE1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_GRENADELAUNCHER') or DeathCauseHash == GetHashKey('WEAPON_RPG') or
+					   DeathCauseHash == GetHashKey('WEAPON_FLAREGUN') or DeathCauseHash == GetHashKey('WEAPON_HOMINGLAUNCHER') or
+					   DeathCauseHash == GetHashKey('WEAPON_FIREWORK') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_HEAVY1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_MINIGUN') then
+					INT = GetRandomIntInRange(0, 3)
+					DeathReason = 'DM_TK_MINI1' .. INT
+				elseif DeathCauseHash == GetHashKey('WEAPON_GRENADE') or DeathCauseHash == GetHashKey('WEAPON_PROXMINE') or
+					   DeathCauseHash == GetHashKey('WEAPON_EXPLOSION') or DeathCauseHash == GetHashKey('WEAPON_STICKYBOMB') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_BOMB1' .. INT
+				elseif DeathCauseHash == GetHashKey('VEHICLE_WEAPON_TANK') then
+					INT = GetRandomIntInRange(0, 2)
+					DeathReason = 'DM_TK_HEAVY1' .. INT
+				elseif DeathCauseHash == GetHashKey('VEHICLE_WEAPON_ROTORS') then
+					DeathReason = 'DM_TK_VEH1'
+				elseif DeathCauseHash == GetHashKey('WEAPON_RUN_OVER_BY_CAR') or DeathCauseHash == GetHashKey('WEAPON_RAMMED_BY_CAR') then
+					DeathReason = 'DM_TK_VK1'
 				else
-					DeathReason = "DM_TICK1"
+					DeathReason = 'DM_TICK1'
 				end
-				DeathReason = GetLabelText(DeathReason):gsub("~a~", GetPlayerName(Killer))
+				DeathReason = GetLabelText(DeathReason):gsub('~a~', GetPlayerName(Killer))
 			end
 			
-			PlaySoundFrontend(-1, "Bed", "WastedSounds", 1)
+			PlaySoundFrontend(-1, 'Bed', 'WastedSounds', 1)
 			
-			ShakeGameplayCam("DEATH_FAIL_IN_EFFECT_SHAKE", 1.0)
-			StartScreenEffect("DeathFailOut", 0, 0)
+			ShakeGameplayCam('DEATH_FAIL_IN_EFFECT_SHAKE', 1.0)
+			StartScreenEffect('DeathFailOut', 0, 0)
 
-			PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-			PushScaleformMovieFunctionParameterString(GetLabelText("RESPAWN_W"))
+			PushScaleformMovieFunction(scaleform, 'SHOW_SHARD_WASTED_MP_MESSAGE')
+			PushScaleformMovieFunctionParameterString(GetLabelText('RESPAWN_W'))
 			PushScaleformMovieFunctionParameterString(DeathReason)
 			PopScaleformMovieFunctionVoid()
 			
@@ -110,7 +120,7 @@ Citizen.CreateThread(function()
 				DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
 			end
 			
-			StopScreenEffect("DeathFailOut")
+			StopScreenEffect('DeathFailOut')
 		end
     end
 end)
--- server/server.lua
+++ server/server.lua
@@ -1,20 +1,24 @@
-local CurrentVersion = "2.0.0"
+local CurrentVersion = '2.1.0'
 
 --Update Check
 
 PerformHttpRequest("https://raw.githubusercontent.com/Flatracer/DeathScreen_Resources/master/VERSION", function(Error, NewestVersion, Header)
-	print("\n")
-	print("####################################################################")
-	print("########################### Death Screen ###########################")
-	print('####################################################################')
-	print('#####                  Current Version: ' .. CurrentVersion .. '                  #####')
-	print('#####                   Newest Version: ' .. NewestVersion .. '                  #####')
-	print('####################################################################')
-	if CurrentVersion ~= NewestVersion then
-		print('##### Outdated, please check the Topic for the newest Version! #####')
-	else
-		print('#####                        Up to date!                       #####')
-	end
-	print('####################################################################')
-	print('\n')
+	PerformHttpRequest("https://raw.githubusercontent.com/Flatracer/DeathScreen_Resources/master/CHANGES", function(Error, Changes, Header)
+		print("\n")
+		print("####################################################################")
+		print("########################### Death Screen ###########################")
+		print('####################################################################')
+		print('#####                  Current Version: ' .. CurrentVersion .. '                  #####')
+		print('#####                   Newest Version: ' .. NewestVersion .. '                  #####')
+		print('####################################################################')
+		if CurrentVersion ~= NewestVersion then
+			print('##### Outdated, please check the Topic for the newest Version! #####')
+			print('####################################################################')
+			print('CHANGES: ' .. Changes)
+		else
+			print('#####                        Up to date!                       #####')
+			print('####################################################################')
+		end
+		print('\n')
+	end)
 end)
